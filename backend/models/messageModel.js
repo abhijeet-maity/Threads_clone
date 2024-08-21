74e4a003ed245d305f74da7d42d6f5340abb8f8b
Abhijeet Maity <abhijeetmaity1000@gmail.com>
2024-08-21 21:46:32 +0530

    added seen field in message model

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -4,6 +4,14 @@ const messageSchema = new mongoose.Schema({
     conversationId: {type: mongoose.Schema.Types.ObjectId, ref: 'Conversation'},
     sender: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     text: String,
+    seen : {
+        type: Boolean,
+        default: false,
+    },
+    img : {
+        type: String,
+        default: "",
+    },
 }, {timestamps: true});
 
 const Message = mongoose.model('Message', messageSchema);
