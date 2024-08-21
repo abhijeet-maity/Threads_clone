efd3480214bddefcd602d3415cd4f9464dee7f37
Abhijeet Maity <abhijeetmaity1000@gmail.com>
2024-08-21 21:52:23 +0530

    implemented online status of user

--- frontend/src/components/Conversation.jsx
+++ frontend/src/components/Conversation.jsx
@@ -1,11 +1,11 @@
-import { Avatar, AvatarBadge, Flex, Image, Stack, Text, useColorModeValue, WrapItem } from '@chakra-ui/react'
+import { Avatar, AvatarBadge, Flex, Image, Stack, Text, Box, useColorModeValue, WrapItem } from '@chakra-ui/react'
 import React from 'react'
 import {BsCheck2All} from "react-icons/bs";
 import userAtom from "../atoms/userAtom";
 import { useRecoilState, useRecoilValue } from 'recoil';
 import {selectedChatAtom} from '../atoms/messagesAtom';
 
-const Conversation = ({conversation}) => {
+const Conversation = ({conversation, isOnline}) => {
 
   const chatPartner = conversation.participants[0];
   const lastMessage = conversation.lastMessage;
@@ -24,11 +24,13 @@ const Conversation = ({conversation}) => {
       userId : chatPartner._id,
       userProfilePic : chatPartner.profilePic,
       username : chatPartner.username,
+      new : conversation.new,
     })} borderRadius={"md"}
     bg={selectedChat?._id === conversation._id ? useColorModeValue("gray.600","gray.dark") : ""}>
       <WrapItem>
         <Avatar size={{base: "xs", sm: "sm", md: "md"}} src={chatPartner?.profilePic}>
-            <AvatarBadge boxSize={"0.8em"} bg={"green.500"}/>
+            {isOnline ? <AvatarBadge boxSize={"0.8em"} bg={"green.500"}/> : ""}
+            {/* <AvatarBadge boxSize={"0.8em"} bg={"green.500"}/> */}
         </Avatar>
       </WrapItem>
 
@@ -37,8 +39,12 @@ const Conversation = ({conversation}) => {
             {chatPartner?.username} <Image src='/verified.png' w={4} h={4} ml={1}/>
         </Text>
         <Text fontSize={"xs"} display={"flex"} alignItems={"center"} gap={1}>
-            {loggedInUser._id === lastMessage.sender ? <BsCheck2All size={15} /> : ""}
-            {lastMessage.text.length > 20 ? lastMessage.text.substring(0, 20) + "..." : lastMessage.text}
+            {loggedInUser._id === lastMessage.sender ? (
+              <Box color={lastMessage.seen ? "blue.400" : ""}>
+                <BsCheck2All size={15}/>
+              </Box>
+            ) : ""}
+            {lastMessage.text.length > 20 ? lastMessage.text.substring(0, 18) + "..." : lastMessage.text}
         </Text>
       </Stack>
 
