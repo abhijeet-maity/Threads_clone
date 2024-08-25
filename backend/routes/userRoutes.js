76278b6caec70acae78394d4dc0f462ce3331a18
Abhijeet Maity <abhijeetmaity1000@gmail.com>
2024-08-25 18:22:11 +0530

    added routes for freeze, getFloowers and get multiple users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const {signupUser, loginUser, logoutUser, followUnfollowUser, updateUser, getUserProfile} = require('../controllers/userController');
+const {signupUser, loginUser, logoutUser, followUnfollowUser, updateUser, getUserProfile, getSuggestedUsers, freeze, getMultipleUsersProfiles, getUserFollowers} = require('../controllers/userController');
 const protectRoute = require('../middlewares/protectRoute');
 const router = express.Router();
 
 
 router.get('/getprofile/:query',getUserProfile);
+router.get('/getMultipleUsersProfiles/:query',getMultipleUsersProfiles);
+router.get('/getfollowers/:query',getUserFollowers);
+router.get('/suggestedusers',protectRoute, getSuggestedUsers);
 router.post('/signup', signupUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
 router.post('/follow/:id', protectRoute, followUnfollowUser);
 router.put('/update/:id',protectRoute, updateUser);
-// router.get('/getprofile/:query',getUserProfile);
+router.put('/freeze',protectRoute, freeze);
+
 
 module.exports = router;
