1d238eeb992ffca5085d10a8b92f0f603554172c
Abhijeet Maity <abhijeetmaity1000@gmail.com>
2024-08-25 18:37:37 +0530

    replaced  custom hooks for logout

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -3,34 +3,18 @@ import { useSetRecoilState } from "recoil";
 import userAtom from "../atoms/userAtom";
 import usePopToast from "../customHooks/usePopToast";
 import { FiLogOut } from "react-icons/fi";
+import useLogout from "../customHooks/useLogout";
+
+
 
 const LogoutButton = () => {
 	const setUser = useSetRecoilState(userAtom);
+	const logout = useLogout();
 	const popToast = usePopToast();
 
-	const handleLogout = async () => {
-		try {
-			const res = await fetch("/api/users/logout", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			});
-			const data = await res.json();
-
-			if (data.error) {
-				popToast("Error", data.error, "error");
-				return;
-			}
-
-			localStorage.removeItem("user-threads");
-			setUser(null);
-		} catch (error) {
-			popToast("Error", error, "error");
-		}
-	};
+	
 	return (
-		<Button position={"relative"} top={"0px"} right={"0px"} ml={0} size={"sm"} onClick={handleLogout}>
+		<Button position={"relative"} top={"0px"} right={"0px"} ml={0} size={"sm"} onClick={logout}>
 			<FiLogOut size={12} />
 			{/* Logout */}
 		</Button>
